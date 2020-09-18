92d38d1473c2ba3cd47ec3ac1cd63b8c55d37831
tleclair1 <54912546+tleclair1@users.noreply.github.com>
2020-09-18 17:13:31 -0600

    created search by ddl

--- ChinookDemo/ChinookSystem/ViewModels/SelectionList.cs
+++ ChinookDemo/ChinookSystem/ViewModels/SelectionList.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace ChinookSystem.ViewModels
 {
-    class dummy
+    public class SelectionList
     {
+        public int ValueId { get; set; }
+
+        public string DisplayText { get; set; }
     }
 }
---
+++ ChinookDemo/WebApp/SamplePages/SearchByDDL.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.ViewModels;
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class SearchByDDL : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "";
+            if (!Page.IsPostBack)
+            {
+                BindArtistList();
+            }
+        }
+
+        protected void BindArtistList()
+        {
+            ArtistController sysmgr = new ArtistController();
+            List<SelectionList> info = sysmgr.Artist_List();
+
+            //info.Sort((x, y) => x.DisplayText.CompareTo(y.DisplayText));
+
+            ArtistList.DataSource = info;
+            ArtistList.DataTextField = nameof(SelectionList.DisplayText);
+            ArtistList.DataValueField = nameof(SelectionList.ValueId);
+            ArtistList.DataBind();
+
+            //ListItem prompt = new ListItem();
+            //prompt.Value = "0";
+            //prompt.Text = "Select an artist...";
+            //ArtistList.Items.Insert(0, prompt);
+
+            ArtistList.Items.Insert(0, new ListItem("Select an artist..."));
+        }
+    }
+} 
