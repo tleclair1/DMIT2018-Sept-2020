2ecd3da5f38b5f8d2168533c10187f8bb22aed29
tleclair1 <54912546+tleclair1@users.noreply.github.com>
2020-09-18 16:43:32 -0600

    created new entities and setup web connection string

---
+++ ChinookDemo/ChinookSystem/Entities/Album.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Albums")]
+    internal class Album
+    {
+        //private data member
+        private string _Title;
+        private string _ReleaseLabel;
+
+        //properties
+
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage = "Album title is required")]
+        [StringLength(160, ErrorMessage = "Album title is limited to 120 characters.")]
+        public string Title { get; set; }
+
+        //[ForeignKey] DO NOT USE!!!
+        public int ArtistId { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage = "Release label is limited to 50 characters.")]
+        public string ReleaseLabel
+        {
+            get { return _ReleaseLabel; }
+            set { _ReleaseLabel = string.IsNullOrEmpty(value) ? null : value; }
+        }
+
+        //navigational properties
+        //use to verlay a model of the database ERD relationships
+        //you need to know the ERD relationship between Table A and Table B
+        //we have a relationship between Artist and Album
+        //that relationship is parent (Artist) to child (Album)
+        //an Album has one parent
+        //an Artist has zero, one or more children
+
+        //the relationship in Album is child to parent (1:1)
+        public virtual Artist Artist { get; set; }
+
+        public virtual ICollection<Track> Tracks { get; set; }
+
+        //constructor 
+
+        //behaviour
+    }
+}
---
+++ ChinookDemo/ChinookSystem/Entities/Track.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Tracks")]
+    internal class Track
+    {
+        [Key]
+        public int TrackId { get; set; }
+
+        [Required(ErrorMessage = "Track name is required")]
+        [StringLength(200, ErrorMessage = "Track name is limited to 200 characters.")]
+        public string Name { get; set; }
+
+        public int? AlbumId { get; set; }
+
+        public int MediaTypeId { get; set; }
+
+        public int? GenreId { get; set; }
+
+        [StringLength(220, ErrorMessage = "Composer is limited to 220 characters.")]
+        public string Composer { get; set; }
+
+        public int Milliseconds { get; set; }
+
+        public int? Bytes { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        public virtual MediaType MediaType { get; set; }
+    }
+}
